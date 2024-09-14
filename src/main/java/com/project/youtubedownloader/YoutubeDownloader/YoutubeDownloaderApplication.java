31c414c06af38d81d845408cd733d429720f2c3f
Jonathan Medina <jonathanmedinah0@gmail.com>
2024-09-13 23:07:59 -0600

    first commit

---
+++ src/main/java/com/project/youtubedownloader/YoutubeDownloader/YoutubeDownloaderApplication.java
@@ -0,0 +1,13 @@
+package com.project.youtubedownloader.YoutubeDownloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeDownloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeDownloaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/youtubedownloader/YoutubeDownloader/YoutubeDownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.youtubedownloader.YoutubeDownloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeDownloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
